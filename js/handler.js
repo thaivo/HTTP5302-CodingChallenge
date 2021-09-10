62ddaf33f5a521b8537f2beb3f172ef1ebd5c40b
thaivo <thai2903@gmail.com>
2021-09-10 11:47:09 -0400

    - Added crown svg file. - Updated handler.js to load team members and transform result to HTML DOM elements. - Updated style for index.html.

--- js/handler.js
+++ js/handler.js
@@ -1,13 +1,70 @@
 window.onload = function(){
-    var url = "http://sandbox.bittsdevelopment.com/code1/fetchemployees.php";
-    var xmlHttpReq = new XMLHttpRequest();
-    var jsonObj;
-    xmlHttpReq.open("GET",url);
-    xmlHttpReq.onreadystatechange = function(){
-        if(xmlHttpReq.readyState === 4 && xmlHttpReq.status === 200){
-            jsonObj = JSON.parse(xmlHttpReq.responseText);
+    
+    //console.log(jsonObj);
+
+
+    var searchBtn = document.getElementById("searchForm");
+    searchBtn.addEventListener("submit",function(e){
+        e.preventDefault();
+        var url = "http://sandbox.bittsdevelopment.com/code1/fetchemployees.php";
+        var xmlHttpReq = new XMLHttpRequest();
+        var jsonObj;
+        xmlHttpReq.open("GET",url);
+        xmlHttpReq.onreadystatechange = function(){
+            if(xmlHttpReq.readyState === 4 && xmlHttpReq.status === 200){
+                jsonObj = JSON.parse(xmlHttpReq.responseText);
+                transformJsonToHTML(jsonObj);
+            }
         }
+        xmlHttpReq.send();
+    })
+}
+
+function transformJsonToHTML(input){
+    for(var i = 1; i < 4; i++){
+        createUserProfile(input[i]);
     }
-    xmlHttpReq.send();
-    console.log(jsonObj);
+}
+
+function createUserProfile(jsonUserObj){
+    var userProfile = document.createElement("div");
+    userProfile.setAttribute("class","employee_profile");
+    if(jsonUserObj.employeeisfeatured == 1){
+        var featureElement = document.createElement("div");
+        featureElement.setAttribute("class","crown");
+        var featuredImage = document.createElement("img");
+        featuredImage.setAttribute("src","image/crown.svg");
+        featuredImage.setAttribute("class","featured");
+        featureElement.appendChild(featuredImage);
+        userProfile.appendChild(featureElement);
+    }
+    var avatar = document.createElement("img");
+    avatar.src = "http://sandbox.bittsdevelopment.com/code1/employeepics/"+jsonUserObj.employeeid+".jpg";
+    avatar.setAttribute("class","avatar");
+
+    userProfile.appendChild(avatar);
+
+    var employeeName = document.createElement("h3");
+    var name = document.createTextNode(jsonUserObj.employeefname+" "+jsonUserObj.employeelname);
+    employeeName.appendChild(name);
+    userProfile.appendChild(employeeName);
+
+    var bioElement = document.createElement("p");
+    var bioTextNode = document.createTextNode(jsonUserObj.employeebio);
+    bioElement.appendChild(bioTextNode);
+    userProfile.appendChild(bioElement);
+
+    if(jsonUserObj.roles.length !== 0){
+        var roleArrays = document.createElement("div");
+        roleArrays.setAttribute("class","flex-container");
+        jsonUserObj.roles.forEach(element => {
+            var roleElement = document.createElement("div");
+            roleElement.style.backgroundColor = element.rolecolor;
+            roleElement.style.color = "azure";
+            roleElement.appendChild(document.createTextNode(element.rolename));
+            roleArrays.appendChild(roleElement);
+        });
+        userProfile.appendChild(roleArrays);
+    }
+    document.getElementById("employees").appendChild(userProfile);
 }
